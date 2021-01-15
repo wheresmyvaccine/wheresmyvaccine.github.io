5851f29a12763a352a2f559810997e228d324f7d
Merlin <merlinpatt@gmail.com>
2021-01-15 13:41:11 -0500

    Update workflow

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,15 @@
 const ghPages = require('gh-pages');
 
-ghPages.publish('build', {}, (error) => {
-  if (error) {
-    console.error(error);
-  }
-});
+ghPages.publish(
+  'build',
+  {
+    repo: `https://${process.env.GITHUB_TOKEN}@github.com/wheresmyvaccine/wheresmyvaccine.github.io.git`,
+    silent: true,
+  },
+  (error) => {
+    if (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+  },
+);
