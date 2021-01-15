76edb3db82f6462ea6ec6a4b608bbefd864b0328
Merlin <merlinpatt@gmail.com>
2021-01-15 13:42:26 -0500

    Update state input

--- src/components/StateInput.js
+++ src/components/StateInput.js
@@ -5,6 +5,11 @@ const StateInput = () => {
     <div>
       <h1>States Length: </h1>
       {stateNames.length}
+      <ul>
+        {stateNames.map((name) => {
+          <li>{name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
