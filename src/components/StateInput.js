e4db00404c05b7e5330462112dc9952705f4fe6c
Merlin <merlinpatt@gmail.com>
2021-01-15 13:09:03 -0500

    Update GitHub Actions

---
+++ src/components/StateInput.js
@@ -0,0 +1,12 @@
+import { stateNames } from '../data';
+
+const StateInput = () => {
+  return (
+    <div>
+      <h1>States Length: </h1>
+      {stateNames.length}
+    </div>
+  );
+};
+
+export default StateInput;
--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,23 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
+import StateInput from './StateInput';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
-)
+  <div id='app'>
+    <h1>Where's my Vaccine?</h1>
+    <input type='text' />
+    <StateInput />
+    <Router>
+      <Home path='/' />
+      <Profile path='/profile/' user='me' />
+      <Profile path='/profile/:user' />
+    </Router>
+  </div>
+);
 
 export default App;
