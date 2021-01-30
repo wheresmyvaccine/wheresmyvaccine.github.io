4c4601f40038901872c4118cdffa4413360a9f03
Merlin <merlinpatt@gmail.com>
2021-01-30 04:00:06 -0500

    Add data for north dakota and texas

--- src/data/states/north_dakota.js
+++ src/data/states/north_dakota.js
@@ -1,4 +1,20 @@
 export default {
+  'genericLivingSettings': [
+
+  ],
+  'livingSettings': {
+
+  },
+  'specificLivingSettings': {
+
+  },
+
+  'genericWorkSettings': [
+
+  ],
+  'specificWorkSettings': {
+
+  },
   'workSettings': {
     'Healthcare': '1a',
     'First Responders': '1a',
@@ -21,51 +37,53 @@ export default {
     'Public transit (bus, taxi, ride-share, etc)': '1c.7',
     'Blood bank workers not previously vaccinated': '1c.9',
     'Information Technology': '1c.10',
-    'All other essential workers per Cybersecurity and Infrastructure Security Agency (CISA)': '1c.11',
+    'All other essential workers per Cybersecurity and Infrastructure Security Agency (CISA)':
+      '1c.11',
   },
+
   'age': {
     '75+': '1b.1',
     '65+': '1b.5',
+  },
+
+  'genericHealthConditions': [
+
+  ],
+  'specificHealthConditions': {
+
   },
   'healthConditions': {
     '2+ high risk medical conditions': '1b.6',
     '1+ high risk medical conditions': '',
     'none': '',
   },
+
   'combinations': [
-    { 'workSetting': '', 'age': '65-74', 'healthConditions': '2+ high risk medical conditions', 'phase': '1b.2' },
-    { 'workSetting': '', 'age': '65+', 'healthConditions': '1+ high risk medical conditions', 'phase': '1b.2' },
-    { 'workSetting': '', 'age': '16-64', 'healthConditions': '1+ high risk medical conditions', 'phase': '1c.8' },
+    {
+      'age': '65-74',
+      'healthConditions': '2+ high risk medical conditions',
+      'phase': '1b.2',
+    },
+    {
+      'age': '65+',
+      'healthConditions': '1+ high risk medical conditions',
+      'phase': '1b.2',
+    },
+    {
+      'age': '16-64',
+      'healthConditions': '1+ high risk medical conditions',
+      'phase': '1c.8',
+    },
   ],
   'phases': {
-    '1a': { 'startDate': '' },
-    '1b': { 'startDate': '' },
+    '1a': { 'startDate': '2021-01-15' },
+    '1b': { 'startDate': '2021-01-15' },
     '1c': { 'startDate': '' },
   },
+  'resources': {
+    'COVID-19 Vaccine Priority Groups':
+      'https://www.health.nd.gov/covid-19-vaccine-priority-groups',
+    'Phase 1B Started':
+      'https://www.newscenter1.tv/north-dakota-entering-2nd-phase-of-vaccine-priority-groups/',
+  },
 };
-
-// Phase 1A
-// North Dakota	1A	Healthcare, First Responders, Long Term Care
-
-// Phase 1B
-// North Dakota	1B	1		75	999
-// North Dakota	1B	2		65	74	2+ high risk medical conditions
-// North Dakota	1B	3	Corrections, Group Homes, Treatment Centers, Homeless Shelters, Other Congregate Settings
-// North Dakota	1B	4		65	999	1+ high risk medical conditions
-// North Dakota	1B	5		65	999
-// North Dakota	1B	6				2+ high risk medical conditions
-// North Dakota	1B	7	Child care workers
-// North Dakota	1B	8	Workers at Preschool through 12th grade
-
-// Phase 1C
-// North Dakota	1C	1	National Guard
-// North Dakota	1C	2	Food access workers (excluding restaurant workers)
-// North Dakota	1C	3	Public safety answering points (911)
-// North Dakota	1C	4	COVID Vaccine Dev/Supply Manufacturing
-// North Dakota	1C	5	Non-1A healthcare/public health workers
-// North Dakota	1C	6	Free standing clinical laundries
-// North Dakota	1C	7	Public transit (bus, taxi, ride-share, etc)
-// North Dakota	1C	8		16	64	1+ high risk medical conditions
-// North Dakota	1C	9	Blood bank workers not previously vaccinated
-// North Dakota	1C	10	Information Technology
-// North Dakota	1C	11	All other essential workers per Cybersecurity and Infrastructure Security Agency (CISA)
--- src/data/states/texas.js
+++ src/data/states/texas.js
@@ -1,7 +1,60 @@
 export default {
-  'workSettings': {},
-  'age': {},
-  'healthConditions': {},
-  'combinations': {},
-  'phases': {},
+  'genericLivingSettings': [
+
+  ],
+  'specificLivingSettings': {
+
+  },
+  'livingSettings': {
+    'long term care facility': '1a',
+  },
+
+  'genericWorkSettings': [
+
+  ],
+  'specificWorkSettings': {
+
+  },
+  'workSettings': {
+    'Healthcare': '1a',
+    'long term care facility': '1a',
+    'emergency medical services': '1a',
+    'home health': '1a',
+    'school nurse': '1a',
+  },
+
+  'age': {
+    '65+': '1b',
+  },
+
+  'genericHealthConditions': [
+    
+  ],
+  'specificHealthConditions': {
+
+  },
+  'healthConditions': {
+    '1+ chronic illness': '',
+  },
+
+  'combinations': [
+    { 'age': '16+', 'healthConditions': '1+ chronic illness', 'phase': '1b' },
+  ],
+  'phases': {
+    '1a': {
+      'startDate': '2021-01-15',
+    },
+    '1b': {
+      'startDate': '2021-01-15',
+    },
+  },
+  'resources': {
+    'COVID-19 Vaccine Information':
+      'https://www.dshs.state.tx.us/coronavirus/immunize/vaccine.aspx',
+  },
 };
+
+// Texas	1A		Healthcare, long term care facility, emergency medical services, home health, school nurse
+// Texas	1A						Long term care facility
+// Texas	1B			65	999
+// Texas	1B			16	999	1+ chronic illness
