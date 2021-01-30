cab384280456b3ee81a7d27dad015a78a11e5794
Merlin <merlinpatt@gmail.com>
2021-01-30 04:02:10 -0500

    Add defaults

--- src/data/stateData.js
+++ src/data/stateData.js
@@ -1,13 +1,30 @@
 import * as stateData from './states';
+import defaults from './states/_defaults';
 
 const normalizedStateData = {};
 
-Object.entries(stateData).forEach(([ state, data ]) => {
+const getSelectorsFromList = (array) => array.map((key) => ({label: key, value: key}));
+
+const getSelectorsFromPhaseMap = (phaseMap) => getSelectorsFromList(Object.keys(phaseMap || {}));
+
+const overrideGenericsIfSet = (data, name) =>
+  data[name].length > 0 ? data[name].length : defaults[name];
+
+Object.entries(stateData).forEach(([state, data]) => {
   normalizedStateData[state] = {
+    ...defaults,
     ...data,
     selectors: {
-      workSettings: Object.keys(data.workSettings || []).map((ws) => ({ label: ws, value: ws })),
-      healthConditions: Object.keys(data.healthConditions || []).map((ws) => ({ label: ws, value: ws })),
+      genericLivingSettings: getSelectorsFromList(
+        overrideGenericsIfSet(data, 'genericLivingSettings'),
+      ),
+      genericWorkSettings: getSelectorsFromList(overrideGenericsIfSet(data, 'genericWorkSettings')),
+      genericHealthConditions: getSelectorsFromList(
+        overrideGenericsIfSet(data, 'genericHealthConditions'),
+      ),
+      livingSettings: getSelectorsFromPhaseMap(data.livingSettings),
+      workSettings: getSelectorsFromPhaseMap(data.workSettings),
+      healthConditions: getSelectorsFromPhaseMap(data.healthConditions),
     },
   };
 });
---
+++ src/data/states/_defaults.js
@@ -0,0 +1,28 @@
+// Generics are probably the only ones needed for defaults.
+// The rest are for completeness.
+export default {
+  'genericLivingSettings': ['Long Term Care Facility'],
+  'specificLivingSettings': {},
+  'livingSettings': {},
+
+  'genericWorkSettings': ['Education', 'Healthcare', 'Public Transit'],
+  'specificWorkSettings': {},
+  'workSettings': {},
+
+  'age': {},
+
+  'genericHealthConditions': ['Immunocompromised'],
+  'specificHealthConditions': {},
+  'healthConditions': {},
+
+  'combinations': {},
+  'phases': {
+    '1a': {
+      'startDate': '',
+    },
+    '1b': {
+      'startDate': '',
+    },
+  },
+  'resources': {},
+};
