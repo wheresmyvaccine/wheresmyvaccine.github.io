68615682b3d6fa7f003e0a1fe766caf5cccea04d
Merlin (they/them) <merlinpatt@gmail.com>
2021-02-16 00:32:18 -0500

    Fix hash app

--- src/HashApp.js
+++ src/HashApp.js
@@ -1,14 +1,12 @@
 import { h } from 'preact';
-// import { Router } from 'preact-router';
+import { Router } from 'preact-router';
 import { createHashHistory } from 'history';
 
 // Code-splitting is automated for `routes` directory
 import StateInput from '~/components/StateInput';
-// import AgeInput from '~/components/AgeInput';
+import AgeInput from '~/components/AgeInput';
 
-let history = createHashHistory();
-
-console.log(history);
+// let history = createHashHistory();
 
 const App = () => (
   <div id='app'>
@@ -23,7 +21,10 @@ const App = () => (
         </li>
       </ul>
     </nav>
-    <StateInput />
+    <Router history={createHashHistory()}>
+      <StateInput path='/state' />
+      <AgeInput path='/age' />
+    </Router>
   </div>
 );
 
